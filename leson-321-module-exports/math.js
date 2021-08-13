d7a0083ed3f6dcfac081d2f5087d299b0c0ba0c7
alex mendoza <tiburon9386@gmail.com>
2021-08-13 08:32:56 -0500

    hice las dos primeras clases iniciando con la 317 corriendo codigo con node desde terminal

---
+++ leson-321-module-exports/math.js
@@ -0,0 +1,11 @@
+const PI = 3.141592;
+
+const square = x => x * x;
+
+const add = (x,y) => x+y;
+
+// podemos requerir estas constantes para usarlas desde otros archivos
+
+// necesitamos darles permiso a las constantes para ser utilizadas en otras aplicaciones
+
+module.exports = "helloooo"
---
+++ lesson-317-rinning-node/app.js
@@ -0,0 +1,5 @@
+for(let i=0; i<10; i++){
+    console.log("Hola hijos de su reputisima madre!!!");
+}
+
+// lo que hice a continuacion fue correr el codigo directo desde terminal con el codigo estando en la carpeta de este codigo: node app.js
