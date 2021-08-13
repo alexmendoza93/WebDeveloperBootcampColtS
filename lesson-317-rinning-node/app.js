bc74a337e8f9676c63128457f81fe1b7be5f1156
alex mendoza <tiburon9386@gmail.com>
2021-08-13 08:35:18 -0500

    combie de frase en app.js

--- lesson-317-rinning-node/app.js
+++ lesson-317-rinning-node/app.js
@@ -1,5 +1,5 @@
 for(let i=0; i<10; i++){
-    console.log("Hola hijos de su reputisima madre!!!");
+    console.log("Hola hijos de su mami!!!");
 }
 
 // lo que hice a continuacion fue correr el codigo directo desde terminal con el codigo estando en la carpeta de este codigo: node app.js
