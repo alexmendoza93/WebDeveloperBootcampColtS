ce986a00467eeaafd34000215fd95a012ceb5b19
alex mendoza <tiburon9386@gmail.com>
2021-08-14 08:04:01 -0500

    se agrego comentario de como hacer carpetas desde cualquier lado

--- crearCarpetasArchivos-319/boilerplate.js
+++ crearCarpetasArchivos-319/boilerplate.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const { createConnection } = require("net");
 // fs es un objeto que tiene multitud de metodos unos de los cuales podemos usar para hacer carpetas y archivos
 const folderName = process.argv[2] || 'project'
 // aque se agarrara el tercer elemento del objeto llamado y si no se pone un argumento da por default project
@@ -15,3 +16,5 @@ try {
 }
 
 // y entonces corremos en nuestra consola en cualquier parte que desemos crear esa carpeta con: node boilerplate.js nomCarpeta
+
+// se puede crear la carpeta con sus archivos desde cualquier carpeta solo poniendo la ruta exacta como: node (estando en carpeta Udemy) practicas/creacionDECarpetasArcchivos/boilerplate.js nomCarpeta
