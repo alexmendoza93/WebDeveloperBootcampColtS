6de56a5a18d85e4182f736949db9ef51aae80e9d
alex mendoza <tiburon9386@gmail.com>
2021-08-14 13:05:07 -0500

    instalamos nuestro segundo paquete de npm pero primero hicimos nuestro archivo json con npm init, los paquetes instalados fueron colors y figlet

---
+++ NPM/artster/index.js
@@ -0,0 +1,35 @@
+const figlet = require('figlet');
+const colors = require('colors');
+// figlet('Que Pasa Mundo!!!', function(err, data) {
+//     if (err) {
+//         console.log('Something went wrong...');
+//         console.dir(err);
+//         return;
+//     }
+//     console.log(data)
+// });
+
+figlet.text('Oli desde el mas alla', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data.brightWhite);
+});
+
+// figlet.fonts(function(err, fonts) {
+//     if (err) {
+//         console.log('something went wrong...');
+//         console.dir(err);
+//         return;
+//     }
+//     console.dir(fonts);
+// });
+// este comando sirve ver las fuentes posibles a poner
