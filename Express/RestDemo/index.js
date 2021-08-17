774a3c736b73c9d5c4a6ec3902d521fb1f2625a5
alex mendoza <tiburon9386@gmail.com>
2021-08-17 13:25:54 -0500

    por fin me salio esta cosa, tuvimos acceso a una base de datos imaginaria y la accesamos para imprimir desde el localhost

---
+++ Express/RestDemo/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+// todo esto es el set que necesitaremos 
+// agregamos a nuestro proyecto y mandamos a llamar express, ejs, y habilitamos rutas absolutas y datos de jason y metodos de get y post
+
+// -----------------------------
+// fingimos una base de datos
+const h = [
+    {
+     username: 'Todd',
+     comment: 'lolololol'   
+    },
+    {
+     username: 'Amy',
+     comment: 'I really don´t like it'   
+    },
+    {
+     username: 'Rich',
+     comment: 'this is great'   
+    },
+    {
+     username: 'Alex',
+     comment: 'great stuff'   
+    }
+];
+
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index',{h})
+});
+
+
+
+app.get('/tacos', (req,res)=>{
+    res.send("GET /tacos response")
+});
+
+app.post('/tacos', (req,res)=>{
+    const {meat, qty}=req.body;
+    res.send(`OK, aquí estan tus ${qty} tacos de ${meat}`)
+});
+
+
+
+app.listen(3000, ()=>{
+    console.log('escuchando desde el puerto 3000')
+});
